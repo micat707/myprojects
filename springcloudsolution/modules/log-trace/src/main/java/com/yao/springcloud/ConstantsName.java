94e937155919138e289275b327324d0c91655c39
micat <547497291@qq.com>
2019-01-05 13:20:54 +0800

    调整webfilter顺序

--- springcloudsolution/modules/log-trace/src/main/java/com/yao/springcloud/ConstantsName.java
+++ springcloudsolution/modules/log-trace/src/main/java/com/yao/springcloud/ConstantsName.java
@@ -1,12 +1,14 @@
 package com.yao.springcloud;
 
 
+import org.springframework.cloud.sleuth.instrument.web.TraceFilter;
 import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 
 public class ConstantsName {
     public static final String CLIENT_IP = "CLIENT_IP";
     /**
-     * CLOSECONTROLLER:  trace filter order顺序
+     * CLOSECONTROLLER:  trace filter order顺序  确保在sluth trace filter（Ordered.HIGHEST_PRECEDENCE  + 5）前
      */
-    public final static int TRACE_FILTERORDER = Ordered.HIGHEST_PRECEDENCE + 6;
+    public final static int TRACE_FILTERORDER = Ordered.HIGHEST_PRECEDENCE  + 4;
 }
